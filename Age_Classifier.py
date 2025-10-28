13b9cd94e52b170d65631217aa54a8791dd65cc2
samuel2028-creator <samuel2028@hmacademy.org>
2025-10-28 19:55:09 +0300

    Create Age_Classifier.py

---
+++ Age_Classifier.py
@@ -0,0 +1,18 @@
+age = int(input("How old are you?"))
+
+def classify_person(age):
+    if 0 <= age <= 12:
+        print('Child')
+    elif 13 <= age <= 17:
+    		print('Teenager')
+    elif 18 <= age <= 64:
+        print('Adult')
+    elif age >= 65:
+        print('Senior')
+    else:
+        print('undefined')
+                
+classify_person(age)
+
+    
+    
