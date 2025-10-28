5aa21397364d3489bf9d46ec740ed59873532da9
samuel2028-creator <samuel2028@hmacademy.org>
2025-10-28 20:33:36 +0300

    Time_of_the-day_determiner.py

---
+++ Time_of_the-day_determiner.py
@@ -0,0 +1,13 @@
+time = int(input('What time is it?'))
+
+if time <= 0 or time > 24:
+    print("Error! You should insert a number between (0 - 24)")
+else:
+    if 5<= time <= 11:
+        print("Its Morning")
+    elif 12<= time <= 16:
+        print("Its Afternoon")
+    elif 17<= time <= 20:
+        print("Its Evening")
+    elif time >= 21 or time <= 4:
+        print("Its Night")
